0e3418270f344b1d86a32f741a9c19849d8627eb
jcaror99 <121507887+jcaror99@users.noreply.github.com>
2023-11-05 03:22:47 -0500

    showSelectPage

--- src/js/cards-home.js
+++ src/js/cards-home.js
@@ -1,55 +1,55 @@
 import '../sass/components/_cards-home.scss';
-import { getInitialMovies, BASE_URL, fetchPageBar, API_KEY } from './fetch';
+import { getInitialMovies, BASE_URL, fetchPageBar } from './fetch';
 
-// export const moviesContainer = document.querySelector('.cards-container');
-// export const preloader = document.getElementById('preloader');
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+const movieGrid = document.querySelector('.movie-grid');
 
-document.addEventListener('DOMContentLoaded', () => {
-  const searchButton = document.getElementById('search-button');
-  const searchInput = document.getElementById('search-input');
-  const movieGrid = document.querySelector('.movie-grid');
+export function getMovies(
+  currentPage = 1,
+  route = 'trending/movie/day',
+  searchTerm = ''
+) {
+  fetchPageBar(currentPage, route, searchTerm)
+    .then(data => displayMovies(data.results))
+    .catch(error => console.error('Error:', error));
+}
 
-  const apiKey = API_KEY;
-
-  function displayMovies(movies) {
-    movieGrid.innerHTML = '';
-    movies.forEach(movie => {
-      const movieCard = document.createElement('div');
-      movieCard.classList.add('movie-card');
-      movieCard.innerHTML = `
+function displayMovies(movies) {
+  movieGrid.innerHTML = '';
+  movies.forEach(movie => {
+    const movieCard = document.createElement('div');
+    movieCard.classList.add('movie-card');
+    movieCard.innerHTML = `
            <img src="${
              'https://image.tmdb.org/t/p/w500/' + movie.poster_path
            }" alt="${movie.title}">
            <h3>${movie.title}</h3>
            <p>${movie.overview}</p>
          `;
-      movieGrid.appendChild(movieCard);
-    });
-  }
-
-  function searchMovies() {
-    const searchTerm = searchInput.value;
-    if (searchTerm) {
-      fetchPageBar(1, '/search/movie', searchTerm)
-        .then(data => displayMovies(data.results))
-        .catch(error => console.error('Error:', error));
-    } else {
-      fetchPageBar(1, 'trending/movie/day', '')
-        .then(data => displayMovies(data.results))
-        .catch(error => console.error('Error:', error));
-    }
-  }
-
-  searchButton.addEventListener('click', searchMovies);
-  searchInput.addEventListener('keydown', event => {
-    if (event.key === 'Enter') {
-      searchMovies();
-    }
+    movieGrid.appendChild(movieCard);
   });
+}
 
-  window.addEventListener('load', () => {
-    fetchPageBar(1, 'trending/movie/day', '')
-      .then(data => displayMovies(data.results))
-      .catch(error => console.error('Error:', error));
-  });
+function searchMovies() {
+  const searchTerm = searchInput.value;
+  if (searchTerm) {
+    getMovies(1, '/search/movie', searchTerm);
+  } else {
+    getMovies(1, 'trending/movie/day', '');
+  }
+}
+
+searchButton.addEventListener('click', searchMovies);
+searchInput.addEventListener('keydown', event => {
+  if (event.key === 'Enter') {
+    searchMovies();
+  }
 });
+
+window.addEventListener('load', () => {
+  getMovies(1, 'trending/movie/day', '');
+});
+
+// export const moviesContainer = document.querySelector('.cards-container');
+// export const preloader = document.getElementById('preloader');
