8ffc94e3a72eae606da579600bd0651cd9029296
Elkin Mejía <111328546+elkime@users.noreply.github.com>
2023-11-06 16:01:26 -0500

    integracion buscador

--- src/js/cards-home.js
+++ src/js/cards-home.js
@@ -5,36 +5,52 @@ import { getInitialMovies, getGenres, getMovieDetails, API_KEY } from './fetch';
 // export const preloader = document.getElementById('preloader');
 
 document.addEventListener('DOMContentLoaded', () => {
-  const searchButton = document.getElementById('search-button');
-  const searchInput = document.getElementById('search-input');
+  const searchForm = document.querySelector('.search-form');
+  const searchInput = document.querySelector('.search-form__input');
   const movieGrid = document.querySelector('.movie-grid');
 
   const apiKey = API_KEY;
 
   function getMovies(url) {
     fetch(url)
-      .then(response => response.json())
-      .then(data => displayMovies(data.results))
-      .catch(error => console.error('Error:', error));
+      .then((response) => response.json())
+      .then((data) => displayMovies(data.results))
+      .catch((error) => console.error('Error:', error));
   }
 
   function displayMovies(movies) {
     movieGrid.innerHTML = '';
-    movies.forEach(movie => {
-      const movieCard = document.createElement('div');
-      movieCard.classList.add('movie-card');
-      movieCard.innerHTML = `
-           <img src="${
-             'https://image.tmdb.org/t/p/w500/' + movie.poster_path
-           }" alt="${movie.title}">
-           <h3>${movie.title}</h3>
-           <p>${movie.overview}</p>
-         `;
-      movieGrid.appendChild(movieCard);
+    movies.forEach((movie) => {
+      // Obtener detalles adicionales de la película
+      fetch(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=${apiKey}`)
+        .then((response) => response.json())
+        .then((movieDetails) => {
+          const movieCard = document.createElement('div');
+          movieCard.classList.add('movie-card');
+          movieCard.innerHTML = `
+            <img src="${
+              'https://image.tmdb.org/t/p/w500/' + movie.poster_path
+            }" alt="${movie.title}">
+            <h3 class="movie-title">${movie.title.toUpperCase()}</h3>
+            <p class="movie-genre">Genre: ${movieDetails.genres
+              .map((genre) => genre.name)
+              .join(', ')}</p>
+            <p class="movie-year">Year: ${
+              movieDetails.release_date
+                ? movieDetails.release_date.substring(0, 4)
+                : 'N/A'
+            }</p>
+          `;
+          movieGrid.appendChild(movieCard);
+        })
+        .catch((error) =>
+          console.error('Error fetching movie details:', error)
+        );
     });
   }
 
-  function searchMovies() {
+  function searchMovies(event) {
+    event.preventDefault(); // Prevenir el envío del formulario
     const searchTerm = searchInput.value;
     if (searchTerm) {
       getMovies(
@@ -47,12 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  searchButton.addEventListener('click', searchMovies);
-  searchInput.addEventListener('keydown', event => {
-    if (event.key === 'Enter') {
-      searchMovies();
-    }
-  });
+  searchForm.addEventListener('submit', searchMovies);
 
   getMovies(
     `https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`
