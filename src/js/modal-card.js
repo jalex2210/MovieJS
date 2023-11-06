91a35aa9647c5e2765eac52df2e6a18c9cba8009
LePande <119473273+LePande@users.noreply.github.com>
2023-11-06 18:12:08 -0500

    Estilos Modal

--- src/js/modal-card.js
+++ src/js/modal-card.js
@@ -23,83 +23,44 @@ export function hideModal(tagToHidden, divClicked) {
 export const createModalCard = divClicked => {
   return fetchPageBar(1, `movie/${divClicked}`, '').then(response => {
     const renderBar = `
-    <ul" class="modal-card__list">
-      <li><img src="https://image.tmdb.org/t/p/w300${response.poster_path}" alt="${response.title}" class="modal-card__img"></li>
-      <li>
+    <ul class="modal-card__list">
+      <li class="modal-poster"><img src="https://image.tmdb.org/t/p/w300${response.poster_path}" alt="${response.title}" class="modal-card__img"></li>
+      <li class="modal-data">
       <ul>
         <li><h2 class="modal-card__title">${response.title}</h2></li>
-        <li> 
-          <ul >
-            <li>Vote / Votes</li>
-            <li>${response.vote_average} / ${response.vote_count}</li>
-            <li>Popularity</li>
-            <li>${response.popularity}</li>
-            <li>Original Title</li>
-            <li>${response.original_title}</li>
-            <li>Genre</li>            
-            <li>text</li>
-          </ul >
+        <li class="info-list-wrap"> 
+        <ul class="info-list">
+        <li>
+          <div class="data">Votes / Vote</div>
+          <div class="value">
+            <span class="votes">${response.vote_average}</span> / <span class="vote">${response.vote_count}</span>
+          </div>
         </li>
-        <li>About</li>
-        <li>${response.overview}</li>
+        <li>
+          <div class="data">Popularity</div>
+          <div class="value">${response.popularity}</div>
+        </li>
+        <li>
+          <div class="data">Original Title</div>
+          <div class="value">${response.original_title}</div>
+        </li>
+        <li>
+          <div class="data">Genre</div>
+          <div class="value">text</div>
+        </li>
+      </ul>
+        </li>
+        <li class="about">About</li>
+        <li class="sinopsis">${response.overview}</li>
+        <ul class="buttons">
+        <li><button class="button add">ADD TO WATCHED</button></li>
+        <li><button class="button queue">ADD TO QUEUE</button></li>
+        </ul>
       </ul >
     </li>
     </ul>
+  
     `;
     return renderBar;
   });
 };
-
-//   //Adding EvenListiner to watched and Que buttons
-//   //Adding logic for their textContent
-//   let watched = [];
-//   const watchedData = JSON.parse(localStorage.getItem('watched'));
-
-//   if (watchedData != null) {
-//     watched.push(...watchedData);
-//   }
-
-//   if (watched.includes(movieID)) {
-//     modalBtnAddWatch.textContent = 'REMOVE FROM WATCHED';
-//     modalBtnAddQue.textContent = 'ALREADY WATCHED';
-//   }
-
-//   modalBtnAddWatch.addEventListener('click', saveToWatched);
-
-//   let que = [];
-//   const queData = JSON.parse(localStorage.getItem('que'));
-
-//   if (queData != null) {
-//     que.push(...queData);
-//   }
-
-//   if (que.includes(movieID)) {
-//     modalBtnAddQue.textContent = 'REMOVE FROM QUE';
-//   }
-
-//   modalBtnAddQue.addEventListener('click', saveToQue);
-
-// const displayMovieInfo = async e => {
-//   const movie_id = e.target.parentElement.getAttribute('id');
-
-//   getMovieDetails(movie_id).then(el => {
-//     createModalCard(el);
-//     modal.parentElement.classList.toggle('is-hidden');
-//   });
-// };
-
-// watchedMoviesContainer.addEventListener('click', displayMovieInfo);
-
-// btnClose.addEventListener('click', hideModal);
-
-// window.addEventListener('keydown', event => {
-//   if (event.key === 'Escape') {
-//     hideModal();
-//   }
-// });
-
-// modal.parentElement.addEventListener('click', el => {
-//   if (el.target.classList.contains('modal-container')) {
-//     hideModal();
-//   }
-// });
