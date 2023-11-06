5ad9463aefe352b9f7618094a8cd9305b6337f20
LePande <119473273+LePande@users.noreply.github.com>
2023-11-06 18:18:07 -0500

    modal estilos

--- src/js/modal-card.js
+++ src/js/modal-card.js
@@ -13,10 +13,11 @@ export function hideModal(tagToHidden, divClicked) {
   const modal = document.querySelector(tagToHidden);
   modal.classList.toggle('is-hidden');
 
-  const modalContainer = modal.querySelector('.modal-card');
-  // console.log(modalContainer);
-  console.log(divClicked);
-  // createModalCard(divClicked);
+  const modalCard = modal.querySelector('.modal-card');
+
+  createModalCard(divClicked).then(renderBar => {
+    return (modalCard.innerHTML = renderBar);
+  });
 }
 
 export const createModalCard = divClicked => {
@@ -63,3 +64,57 @@ export const createModalCard = divClicked => {
     return renderBar;
   });
 };
+
+//   //Adding EvenListiner to watched and Que buttons
+//   //Adding logic for their textContent
+//   let watched = [];
+//   const watchedData = JSON.parse(localStorage.getItem('watched'));
+
+//   if (watchedData != null) {
+//     watched.push(...watchedData);
+//   }
+
+//   if (watched.includes(movieID)) {
+//     modalBtnAddWatch.textContent = 'REMOVE FROM WATCHED';
+//     modalBtnAddQue.textContent = 'ALREADY WATCHED';
+//   }
+
+//   modalBtnAddWatch.addEventListener('click', saveToWatched);
+
+//   let que = [];
+//   const queData = JSON.parse(localStorage.getItem('que'));
+
+//   if (queData != null) {
+//     que.push(...queData);
+//   }
+
+//   if (que.includes(movieID)) {
+//     modalBtnAddQue.textContent = 'REMOVE FROM QUE';
+//   }
+
+//   modalBtnAddQue.addEventListener('click', saveToQue);
+
+// const displayMovieInfo = async e => {
+//   const movie_id = e.target.parentElement.getAttribute('id');
+
+//   getMovieDetails(movie_id).then(el => {
+//     createModalCard(el);
+//     modal.parentElement.classList.toggle('is-hidden');
+//   });
+// };
+
+// watchedMoviesContainer.addEventListener('click', displayMovieInfo);
+
+// btnClose.addEventListener('click', hideModal);
+
+// window.addEventListener('keydown', event => {
+//   if (event.key === 'Escape') {
+//     hideModal();
+//   }
+// });
+
+// modal.parentElement.addEventListener('click', el => {
+//   if (el.target.classList.contains('modal-container')) {
+//     hideModal();
+//   }
+// });
