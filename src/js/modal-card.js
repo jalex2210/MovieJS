268e5a358ab712530cfe5ba5f1a33d71977cfa7a
jcaror99 <121507887+jcaror99@users.noreply.github.com>
2023-11-06 19:31:17 -0500

    closeButton

--- src/js/modal-card.js
+++ src/js/modal-card.js
@@ -7,8 +7,6 @@ import { movieID, getMovieDetails, fetchPageBar } from './fetch.js';
 // import { after } from 'lodash';
 //import { merge } from 'lodash';
 
-// const btnClose = modal.querySelector('.btn--close');
-
 export function hideModal(tagToHidden, divClicked) {
   const modal = document.querySelector(tagToHidden);
   modal.classList.toggle('is-hidden');
@@ -23,6 +21,7 @@ export function hideModal(tagToHidden, divClicked) {
 export const createModalCard = divClicked => {
   return fetchPageBar(1, `movie/${divClicked}`, '').then(response => {
     const renderBar = `
+    <button class="btn btn--close">X</button>
     <ul class="modal-card__list">
       <li class="modal-poster"><img src="https://image.tmdb.org/t/p/w300${response.poster_path}" alt="${response.title}" class="modal-card__img"></li>
       <li class="modal-data">
@@ -59,62 +58,17 @@ export const createModalCard = divClicked => {
       </ul >
     </li>
     </ul>
-  
     `;
     return renderBar;
   });
 };
 
-//   //Adding EvenListiner to watched and Que buttons
-//   //Adding logic for their textContent
-//   let watched = [];
-//   const watchedData = JSON.parse(localStorage.getItem('watched'));
+// const btnClose = modal.querySelector('.btn--close');
 
-//   if (watchedData != null) {
-//     watched.push(...watchedData);
-//   }
+// btnClose.addEventListener('click', e => {
+//   console.log(e);
+//   // if (e.target.tagName === 'A') {
+//   //   loaderModal.classList.toggle('is-hidden')
 
-//   if (watched.includes(movieID)) {
-//     modalBtnAddWatch.textContent = 'REMOVE FROM WATCHED';
-//     modalBtnAddQue.textContent = 'ALREADY WATCHED';
-//   }
-
-//   modalBtnAddWatch.addEventListener('click', saveToWatched);
-
-//   let que = [];
-//   const queData = JSON.parse(localStorage.getItem('que'));
-
-//   if (queData != null) {
-//     que.push(...queData);
-//   }
-
-//   if (que.includes(movieID)) {
-//     modalBtnAddQue.textContent = 'REMOVE FROM QUE';
-//   }
-
-//   modalBtnAddQue.addEventListener('click', saveToQue);
-
-// const displayMovieInfo = async e => {
-//   const movie_id = e.target.parentElement.getAttribute('id');
-
-//   getMovieDetails(movie_id).then(el => {
-//     createModalCard(el);
-//     modal.parentElement.classList.toggle('is-hidden');
-//   });
-// };
-
-// watchedMoviesContainer.addEventListener('click', displayMovieInfo);
-
-// btnClose.addEventListener('click', hideModal);
-
-// window.addEventListener('keydown', event => {
-//   if (event.key === 'Escape') {
-//     hideModal();
-//   }
-// });
-
-// modal.parentElement.addEventListener('click', el => {
-//   if (el.target.classList.contains('modal-container')) {
-//     hideModal();
-//   }
+//   // }
 // });
