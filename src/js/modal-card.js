1d4b759223c7d25fbea9ea9282faf11af4279709
jalex2210 <alexbenitez.rdm@gmail.com>
2023-11-06 19:37:55 -0500

    modal cards

--- src/js/modal-card.js
+++ src/js/modal-card.js
@@ -7,40 +7,54 @@ import { movieID, getMovieDetails, fetchPageBar } from './fetch.js';
 // import { after } from 'lodash';
 //import { merge } from 'lodash';
 
-// const btnClose = modal.querySelector('.btn--close');
-
 export function hideModal(tagToHidden, divClicked) {
   const modal = document.querySelector(tagToHidden);
   modal.classList.toggle('is-hidden');
 
-  const modalContainer = modal.querySelector('.modal-card');
-  // console.log(modalContainer);
-  console.log(divClicked);
-  // createModalCard(divClicked);
+  const modalCard = modal.querySelector('.modal-card');
+
+  createModalCard(divClicked).then(renderBar => {
+    return (modalCard.innerHTML = renderBar);
+  });
 }
 
 export const createModalCard = divClicked => {
   return fetchPageBar(1, `movie/${divClicked}`, '').then(response => {
     const renderBar = `
-    <ul" class="modal-card__list">
-      <li><img src="https://image.tmdb.org/t/p/w300${response.poster_path}" alt="${response.title}" class="modal-card__img"></li>
-      <li>
+    <button class="btn btn--close">X</button>
+    <ul class="modal-card__list">
+      <li class="modal-poster"><img src="https://image.tmdb.org/t/p/w300${response.poster_path}" alt="${response.title}" class="modal-card__img"></li>
+      <li class="modal-data">
       <ul>
         <li><h2 class="modal-card__title">${response.title}</h2></li>
-        <li> 
-          <ul >
-            <li>Vote / Votes</li>
-            <li>${response.vote_average} / ${response.vote_count}</li>
-            <li>Popularity</li>
-            <li>${response.popularity}</li>
-            <li>Original Title</li>
-            <li>${response.original_title}</li>
-            <li>Genre</li>            
-            <li>text</li>
-          </ul >
+        <li class="info-list-wrap"> 
+        <ul class="info-list">
+        <li>
+          <div class="data">Votes / Vote</div>
+          <div class="value">
+            <span class="votes">${response.vote_average}</span> / <span class="vote">${response.vote_count}</span>
+          </div>
         </li>
-        <li>About</li>
-        <li>${response.overview}</li>
+        <li>
+          <div class="data">Popularity</div>
+          <div class="value">${response.popularity}</div>
+        </li>
+        <li>
+          <div class="data">Original Title</div>
+          <div class="value">${response.original_title}</div>
+        </li>
+        <li>
+          <div class="data">Genre</div>
+          <div class="value">text</div>
+        </li>
+      </ul>
+        </li>
+        <li class="about">About</li>
+        <li class="sinopsis">${response.overview}</li>
+        <ul class="buttons">
+        <li><button class="button add">ADD TO WATCHED</button></li>
+        <li><button class="button queue">ADD TO QUEUE</button></li>
+        </ul>
       </ul >
     </li>
     </ul>
@@ -49,56 +63,12 @@ export const createModalCard = divClicked => {
   });
 };
 
-//   //Adding EvenListiner to watched and Que buttons
-//   //Adding logic for their textContent
-//   let watched = [];
-//   const watchedData = JSON.parse(localStorage.getItem('watched'));
+// const btnClose = modal.querySelector('.btn--close');
 
-//   if (watchedData != null) {
-//     watched.push(...watchedData);
-//   }
+// btnClose.addEventListener('click', e => {
+//   console.log(e);
+//   // if (e.target.tagName === 'A') {
+//   //   loaderModal.classList.toggle('is-hidden')
 
-//   if (watched.includes(movieID)) {
-//     modalBtnAddWatch.textContent = 'REMOVE FROM WATCHED';
-//     modalBtnAddQue.textContent = 'ALREADY WATCHED';
-//   }
-
-//   modalBtnAddWatch.addEventListener('click', saveToWatched);
-
-//   let que = [];
-//   const queData = JSON.parse(localStorage.getItem('que'));
-
-//   if (queData != null) {
-//     que.push(...queData);
-//   }
-
-//   if (que.includes(movieID)) {
-//     modalBtnAddQue.textContent = 'REMOVE FROM QUE';
-//   }
-
-//   modalBtnAddQue.addEventListener('click', saveToQue);
-
-// const displayMovieInfo = async e => {
-//   const movie_id = e.target.parentElement.getAttribute('id');
-
-//   getMovieDetails(movie_id).then(el => {
-//     createModalCard(el);
-//     modal.parentElement.classList.toggle('is-hidden');
-//   });
-// };
-
-// watchedMoviesContainer.addEventListener('click', displayMovieInfo);
-
-// btnClose.addEventListener('click', hideModal);
-
-// window.addEventListener('keydown', event => {
-//   if (event.key === 'Escape') {
-//     hideModal();
-//   }
-// });
-
-// modal.parentElement.addEventListener('click', el => {
-//   if (el.target.classList.contains('modal-container')) {
-//     hideModal();
-//   }
+//   // }
 // });
